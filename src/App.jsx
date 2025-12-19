a97f6eaeb00adc4438d8181aba226c2995c2306e
Rafael Medina <rafaelmedina350@hotmail.com>
2025-12-19 06:38:19 -0300

    Feat: Implementación de React Router y navegación dinámica en NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-      <ItemListContainer greeting="¡Bienvenidos a mi E-commerce!" />
-    </>
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting='Todos los productos' />} />
+        <Route path='/category/:categoryId' element={<ItemListContainer greeting='Productos filtrados' />} />
+        <Route path='/item/:itemId' element={<h1>Detalle del producto</h1>} />
+        <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,24 @@
 import CartWidget from '../CartWidget/CartWidget';
+import { NavLink, Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">MercadoSalta</a>
+                <Link className="navbar-brand" to='/'>MercadoSalta</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Tecnología</a>
+                            <NavLink to='/category/tecnologia' className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Tecnología</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Ropa</a>
+                            <NavLink to='/category/ropa' className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Ropa</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Deportes</a>
+                            <NavLink to='/category/deportes' className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Deportes</NavLink>
                         </li>
                     </ul>
                     <CartWidget />
